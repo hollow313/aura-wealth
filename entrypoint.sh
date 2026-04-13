0f614ff82cd11c2ec0a985538a2d9a2cea516068
Hollow <hollow313@users.noreply.github.com>
2026-04-13 18:55:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,24 +3,21 @@ set -e
 
 PGDATA="/var/lib/postgresql/data"
 
-echo "🔍 Démarrage de l'application..."
+echo "🌌 Aura Wealth - Démarrage du système..."
 
+# Si le dossier est vide, on initialise
 if [ ! -d "$PGDATA/base" ]; then
-    echo "🚀 Initialisation de la base de données Aura Wealth..."
+    echo "🚀 Création de la base de données pour l'utilisateur $(id -un)..."
     
-    # Appel direct grâce aux raccourcis globaux
     initdb -U postgres -D "$PGDATA"
     
-    # Démarrage temporaire
     pg_ctl -D "$PGDATA" -w start
     
-    echo "🔐 Configuration des accès SQL..."
     psql -U postgres -c "ALTER USER postgres WITH PASSWORD '$POSTGRES_PASSWORD';"
     psql -U postgres -c "CREATE DATABASE aura_db;"
     
-    # Arrêt propre
     pg_ctl -D "$PGDATA" -m fast stop
 fi
 
-echo "✅ Prêt ! Lancement de l'Aura via Supervisord..."
+echo "✅ Configuration terminée. Lancement d'Aura Wealth..."
 exec /usr/bin/supervisord -c /app/supervisord.conf
